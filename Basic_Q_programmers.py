3deb7bf714fc2d3e7bf2f786ca3d87adc456abc4
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2022-05-18 23:11:48 +0900

    Update Basic_Q_programmers.py

--- Basic_Q_programmers.py
+++ Basic_Q_programmers.py
@@ -1,27 +1,7 @@
-'''
-Q4)
-문자열 내 p와 y의 개수
-문제 설명
-대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
 
-예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+#Q5.
 
-제한사항
-문자열 s의 길이 : 50 이하의 자연수
-문자열 s는 알파벳으로만 이루어져 있습니다.
-입출력 예
-s	answer
-"pPoooyY"	true
-"Pyy"	false
-입출력 예 설명
-입출력 예 #1
-'p'의 개수 2개, 'y'의 개수 2개로 같으므로 true를 return 합니다.
-
-입출력 예 #2
-'p'의 개수 1개, 'y'의 개수 2개로 다르므로 false를 return 합니다.
-'''
-
-# 내가 풀 것
+# 풀기
 def solution(s):
     result = True
     S = s.upper()
@@ -35,3 +15,46 @@ def solution(s):
 def solution(s):
 
     return s.lower().count('p') == s.lower().count('y')
+
+
+#Q6. //
+def no_continuous(s):
+    a = []
+    for i in s:
+        if a[-1:] == [i]: continue
+        a.append(i)
+    return a
+
+
+def solution(arr):
+    answer = []
+    k = -1
+    for i in arr:
+        if i != k:
+            k = i
+            answer.append(i)
+return answer
+
+# Q7.
+
+# 풀기
+def solution(s):
+    num = len(s)
+
+    if num % 2 == 1:
+        answer = s[num//2]
+    else:
+        answer = s[(num//2)-1:(num//2)+1]
+
+    return answer
+
+# 최상
+def string_middle(str):
+    # 함수를 완성하세요
+
+    return str[(len(str)-1)//2:len(str)//2+1]
+
+
+    
+
+
