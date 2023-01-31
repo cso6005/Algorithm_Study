5a2ab866766d6fc0176edb39195343c6b4a63b4c
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-01-31 23:32:12 +0900

    [ADD] BAEKJOON

---
+++ BAEKJOON/Basic1-11단계/기본 수학1/2775.py
@@ -0,0 +1,7 @@
+import sys
+for _ in range(int(sys.stdin.readline().strip())):
+    k, n = map( int, [sys.stdin.readline().strip(), sys.stdin.readline().strip()])
+    
+
+
+# https://www.acmicpc.net/board/view/103418
